e1d1a032dca46b0aebb5d5b9bf75e53732319d58
SunitaKumari <SunitaKumari@DESKTOP-8FM2397.Dlink>
2020-03-03 15:29:27 +0530

    Sample Test Case

--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -6,11 +6,13 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.training.generics.ScreenShot;
+import com.training.pom.HomePOM;
 import com.training.pom.LoginPOM;
 import com.training.utility.DriverFactory;
 import com.training.utility.DriverNames;
@@ -20,35 +22,39 @@ public class LoginTests {
 	private WebDriver driver;
 	private String baseUrl;
 	private LoginPOM loginPOM;
+	private HomePOM homePOM;
 	private static Properties properties;
 	private ScreenShot screenShot;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws IOException {
+	public  void setUpBeforeClass() throws IOException {
 		properties = new Properties();
 		FileInputStream inStream = new FileInputStream("./resources/others.properties");
 		properties.load(inStream);
+		
 	}
 
 	@BeforeMethod
 	public void setUp() throws Exception {
 		driver = DriverFactory.getDriver(DriverNames.CHROME);
-		loginPOM = new LoginPOM(driver); 
+		loginPOM = new LoginPOM(driver);
+		homePOM = new HomePOM(driver); 
 		baseUrl = properties.getProperty("baseURL");
 		screenShot = new ScreenShot(driver); 
 		// open the browser 
 		driver.get(baseUrl);
 	}
 	
-	@AfterMethod
+	@AfterTest
 	public void tearDown() throws Exception {
 		Thread.sleep(1000);
 		driver.quit();
 	}
 	@Test
 	public void validLoginTest() {
-		loginPOM.sendUserName("admin");
-		loginPOM.sendPassword("admin@123");
+		homePOM.clickUserIcone();
+		loginPOM.sendUserName("natarajan.ramanathan93@gmail.com");
+		loginPOM.sendPassword("Welcome@123");
 		loginPOM.clickLoginBtn(); 
 		screenShot.captureScreenShot("First");
 	}
