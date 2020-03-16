aa71260f87b135e37bb52781f23650d03ce5fc21
SunitaKumari <SunitaKumari@DESKTOP-8FM2397.Dlink>
2020-03-16 09:36:44 +0530

    SimpleMedium&Complex TestCases

--- final-framework-testng/src/com/training/pom/UniformPOM.java
+++ final-framework-testng/src/com/training/pom/UniformPOM.java
@@ -14,7 +14,170 @@ public class UniformPOM {
 		this.driver = driver; 
 		PageFactory.initElements(driver, this);
 	}
+		
+	@FindBy(id="input-email")
+	private WebElement email; 
 	
-
+	@FindBy(id="input-password")
+	private WebElement password;
 	
+	@FindBy(xpath="//input[@class='btn btn-primary']")
+	private WebElement loginBtn;
+	
+	@FindBy(className="fa-user")
+	private WebElement userIcone;
+	
+	public void clickUserIcone() {
+		this.userIcone.click(); 
+	}
+	
+	@FindBy(linkText="Login")
+	private WebElement login;
+	
+	public void clickLogin() {
+		this.login.click(); 
+	}
+	
+	public void sendEMailAddress(String email) {
+		this.email.clear();
+		this.email.sendKeys(email);
+	}
+	
+	public void sendPassword(String password) {
+		this.password.clear(); 
+		this.password.sendKeys(password); 
+	}
+	
+	public void clickLoginBtn() {
+		this.loginBtn.click(); 
+	}
+	
+	@FindBy(linkText="Uniform Store")
+	private WebElement uniformStore;
+	
+	public void clickUniformStore() {
+		this.uniformStore.click(); 
+	}
+	
+	@FindBy(xpath="//a[contains(text(),'REGULAR T-SHIRTS (Rust)')]")
+	private WebElement regularTShirt;
+	
+	public void clickTShirt() {
+		this.regularTShirt.click(); 
+	}
+	
+	
+	@FindBy(id="input-option376")
+	private WebElement chestSize;
+	
+	public void clickChestSize() {
+		this.chestSize.click(); 
+	}
+	
+	@FindBy(id="button-cart")
+	private WebElement addCart;
+	
+	public void clickAddToCart() {
+		this.addCart.click(); 
+	}
+		
+	@FindBy(id="cart")
+	private WebElement Cart;
+		
+	public void clickCart() {
+		this.Cart.click();	
+	}
+	
+	@FindBy(partialLinkText="View Cart")
+	private WebElement viewCart;
+		
+	public void clickViewCart() {
+		this.viewCart.click();	
+	}
+	
+	@FindBy(linkText="Checkout")
+	private WebElement checkOut;
+		
+	public void clickCheckOut() {
+		this.checkOut.click();	
+	}
+	
+	
+	@FindBy(id="button-payment-address")
+	private WebElement BillContinue;
+		
+	public void clickBillContinue() {
+		this.BillContinue.click();	
+	}
+	
+	@FindBy(id="button-shipping-address")
+	private WebElement DeliveryContinue;
+		
+	public void clickDeliveryDetailsContinue() {
+		this.DeliveryContinue.click();	
+	}
+	
+	@FindBy(xpath="//textarea[@name='comment']")
+	private WebElement comments;
+	
+	public void sendAddComments(String comments) {
+		this.comments.sendKeys(comments);
+	}
+	
+	@FindBy(id="button-shipping-method")
+	private WebElement ShippingContinue;
+		
+	public void clickDeliveryMethodContinue() {
+		this.ShippingContinue.click();	
+	}
+	
+	@FindBy(name="agree")
+	private WebElement checkBox;
+		
+	public void clickTermConditions() {
+		this.checkBox.click();	
+	}
+	
+	@FindBy(id="button-payment-method")
+	private WebElement PaymentsContinue;
+		
+	public void clickPaymentContinue() {
+		this.PaymentsContinue.click();	
+	}
+	
+	@FindBy(id="button-confirm")
+	private WebElement confirmOrder;
+		
+	public void clickConfirmOrder() {
+		this.confirmOrder.click();	
+	}
+	
+	@FindBy(xpath="//div[@class='container-fluid']//div[2]//div[1]//div[3]//a[1]")
+	private WebElement saleIcon;
+		
+	public void clicksaleIcon() {
+		this.saleIcon.click();	
+	}
+	
+	@FindBy(className="fa-eye")
+	private WebElement viewIcon;
+		
+	public void clickviewIcon() {
+		this.viewIcon.click();	
+	}
+	
+	@FindBy(id="input-order-status")
+	private WebElement orderStatus;
+		
+	public void clickOrderStatus() {
+		this.orderStatus.click();	
+	}
+	
+	@FindBy(id="button-history")
+	private WebElement addHistory;
+		
+	public void clickAddHistory() {
+		this.addHistory.click();	
+	}
 }
+
--- final-framework-testng/tests/com/training/sanity/tests/ComplexUniformTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/ComplexUniformTests.java
@@ -22,6 +22,7 @@ import com.training.generics.ScreenShot;
 import com.training.pom.HomePOM;
 import com.training.pom.LoginPOM;
 import com.training.pom.ProductPOM;
+import com.training.pom.UniformPOM;
 import com.training.utility.DriverFactory;
 import com.training.utility.DriverNames;
 
@@ -31,6 +32,7 @@ public class ComplexUniformTests {
 	private String baseUrl;
 	private LoginPOM loginPOM;
 	private ProductPOM productPOM;
+	private UniformPOM uniformPOM;
 	private static Properties properties;
 	private ScreenShot screenShot;
 
@@ -43,10 +45,12 @@ public class ComplexUniformTests {
 		driver = DriverFactory.getDriver(DriverNames.CHROME);
 		loginPOM = new LoginPOM(driver);
 		productPOM = new ProductPOM(driver);
-		baseUrl = properties.getProperty("baseURL");
+		uniformPOM = new UniformPOM(driver);
+		//baseUrl = properties.getProperty("baseURL");		
 		screenShot = new ScreenShot(driver); 
 		// open the browser 
-		driver.get(baseUrl);
+		//driver.get(baseUrl);
+		driver.get(properties.getProperty("uniformURL"));
 		
 	}
 
@@ -60,12 +64,92 @@ public class ComplexUniformTests {
 	@Test (priority=10)
 	public void validLoginTest(){
 		
+		uniformPOM.clickUserIcone();
+		uniformPOM.clickLogin();
+		uniformPOM.sendEMailAddress("chk4green@gmail.com");
+		uniformPOM.sendPassword("IBM_SPB14");
+		uniformPOM.clickLoginBtn();
+
+	}
+	
+	@Test (priority=20)
+	public void shopUniformsTest_UNF_070() throws Exception{
+			
+		uniformPOM.clickUniformStore();
+		Thread.sleep(1000);
+		uniformPOM.clickTShirt();
+		
+		Thread.sleep(1000);
+		uniformPOM.clickChestSize();
+		
+		WebElement chestSize = driver.findElement(By.id("input-option376"));
+		Select Size = new Select(chestSize);
+		Size.selectByValue("971");
+		Thread.sleep(1000);
+		uniformPOM.clickAddToCart();
+		
+		assertEquals(driver.findElement(By.className("alert-success")).getText(),"Success: You have added REGULAR T-SHIRTS (Rust) to your shopping cart!\n" + 
+				"×");
+		
+		uniformPOM.clickCart();
+		uniformPOM.clickViewCart();
+		
+		assertEquals(driver.findElement(By.partialLinkText("REGULAR T-SHIRTS (Rust)")).getText(),"REGULAR T-SHIRTS (Rust)");
+		uniformPOM.clickCheckOut();
+				
+		uniformPOM.clickBillContinue();
+		Thread.sleep(1000);
+		
+		uniformPOM.clickDeliveryDetailsContinue();
+		Thread.sleep(1000);
+		
+		uniformPOM.sendAddComments("Quality product");
+		uniformPOM.clickDeliveryMethodContinue();
+		
+		Thread.sleep(1000);
+		uniformPOM.clickTermConditions();
+		uniformPOM.clickPaymentContinue();
+		
+		uniformPOM.clickConfirmOrder();
+		
+		assertEquals(driver.findElement(By.xpath("//h1[contains(text(),'Your order has been placed!')]")).getText(),"YOUR ORDER HAS BEEN PLACED!");
+		
+		driver.get(properties.getProperty("baseURL"));
 		loginPOM.sendUserName("admin");
 		loginPOM.sendPassword("admin@123");
 		loginPOM.clickLoginBtn(); 
+		
+		uniformPOM.clicksaleIcon();
+		
+		assertEquals(driver.findElement(By.linkText("Customer")).getText(),"Customer");
+		assertEquals(driver.findElement(By.linkText("Status")).getText(),"Status");
+		
+		uniformPOM.clickviewIcon();
+		
+		assertEquals(driver.findElement(By.xpath("//td[contains(text(),'Payment Address')]")).getText(),"Payment Address");
+		assertEquals(driver.findElement(By.xpath("//td[contains(text(),'Shipping Address')]")).getText(),"Shipping Address");
+		
+		uniformPOM.clickOrderStatus();
+		
+		WebElement orderStatus = driver.findElement(By.id("input-order-status"));
+		Select Status = new Select(orderStatus);
+		Status.selectByVisibleText("Complete");
+		
+		uniformPOM.clickAddHistory();
+		
+		screenShot.captureScreenShot("Seventh-C");
 	}
 
-
+	@Test (priority=30)
+	public void productRewardsPointTest_UNF_071() {
+		
+		loginPOM.clickProduct();
+		assertEquals(driver.getTitle(),"Products");
+		
+		loginPOM.AddProduct();
+		assertEquals(driver.findElement(By.className("panel-title")).getText(),"Add Product");
+	}
+	
 	
 
 	
